9d87989f206747e857227f65fcca8977bef0c119
StephyStuff <stephanie.birch13@gmail.com>
2016-11-01 17:55:04 -0400

    Added a second function for creating the li, img, and p tags and appending to the ul.

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,15 @@
 (function (){
+  window.ns = window.ns || {};
 
+  var something = function addOneItemToUl(){
+    var listItem = document.createElement('li');
+    var image = document.createElement('img');
+    listItem.appendChild(image)
+    var paragraph = document.createElement('p');
+    listItem.appendChild(paragraph)
 
+    document.querySelector('ul').appendChild(listItem);
+  }
 
-
+window.ns.addOneItemToUl = something
 })();
