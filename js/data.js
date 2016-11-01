6b275a0d55d9c3ff07bbed7de5eb6a258340afc9
StephyStuff <stephanie.birch13@gmail.com>
2016-11-01 17:53:35 -0400

    Added a second function for creating the li, img, and p tags and appending to the ul.

--- js/data.js
+++ js/data.js
@@ -1,7 +1,31 @@
 (function(){
+  window.ns = window.ns || {};
 
+  $('button')
+  .on('click', function getAuthorization (eventObj){
+    var myToken = $('input').val();
+    $.ajax({
+      url: 'https://api.github.com/users/rposborne/orgs',
+      dataType: 'json',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'token ' + myToken
+      }
+    })
+    .done(function handleSuccess(data){
+      $.each(data, function addOneItemToUl(each){
 
+      })
+      // data.forEach(function addOneItemToUl(each){
+      //   window.ns.addOneItemToUl();
+      //   console.log(each);
+      //})
 
+    })
+    .fail(function handleFailure(xhr) {
+      // console.log(xhr);
+    })
+  })
 
 
 
